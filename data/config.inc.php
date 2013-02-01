4b656b6e73a545c04bf2612278fd1049409ee5de
alin40404 <302592040@qq.com>
2013-02-01 17:56:53 +0800

    first commit

---
+++ data/config.inc.php
@@ -0,0 +1,18 @@
+<?php 
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '40404chen');
+define('UC_DBNAME', 'ucenter');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', 'uc_');
+define('UC_COOKIEPATH', '/');
+define('UC_COOKIEDOMAIN', '');
+define('UC_DBCONNECT', 0);
+define('UC_CHARSET', 'utf-8');
+define('UC_FOUNDERPW', 'fc7fda0922dc2fb9c3bb718fe56d5fcf');
+define('UC_FOUNDERSALT', '247365');
+define('UC_KEY', '90aZ8y1qbE727X6l9E9uf9aW3Pfq7T4q5D3g8yaT9L3C738N3c6rfde993bQ8idT');
+define('UC_SITEID', '93a78w1FbR717r6k9V9nfmar35fV7n4g5i338ea19D3q7H8P3J6NfeeW9Nbb88dp');
+define('UC_MYKEY', '91aA8i1sbl7e7w6q9t9VfVa53OfB7a4O5k3B89an9i347Y8f3G6vfNe19Kbu8md2');
+define('UC_DEBUG', false);
+define('UC_PPP', 20);
---
+++ data/logs/201301.php
@@ -0,0 +1,9 @@
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1357628187	login	succeed
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1357628300	user_add	username=alin
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1357628327	admin_add	username=alin
+<?PHP exit;?>		127.0.0.1	1357628569	logout	
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1357629827	login	succeed
+<?PHP exit;?>		127.0.0.1	1357631768	logout	
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1357712854	login	succeed
+<?PHP exit;?>		127.0.0.1	1357713279	logout	
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1357713366	login	succeed
